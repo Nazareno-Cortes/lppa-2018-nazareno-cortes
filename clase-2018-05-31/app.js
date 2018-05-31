f5cf3fe00896edd283a15c9955f51d75d2b0c146
Nazareno Cortes <naza5cortes@gmail.com>
2018-05-31 12:05:55 -0300

    Changn Color

--- clase-2018-05-31/app.js
+++ clase-2018-05-31/app.js
@@ -1,1 +1,14 @@
 console.log("Hello");
+
+var changeColor= function (li) {
+  li.style.backgroundColor = '#f00';
+}
+window.onload = function () {
+  var btnPlay = document.getElementById('play');
+  btnPlay.onclick = function () {
+    var liAlives = document.getElementsByClassName('alive');
+    for (var i = 0; i < liAlives.length; i++) {
+     changeColor(liAlives[i]);
+   }//fin for liAlives
+  }
+}//fin de la funcion window.onload
